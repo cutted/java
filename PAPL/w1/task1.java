6da60c21b8d7536a2921260fc31e0b4738bb1ac4
Jono <30672425+cutted@users.noreply.github.com>
2019-11-08 13:48:31 +0000

    Create task1.java

---
+++ PAPL/w1/task1.java
@@ -0,0 +1,11 @@
+public class task1 {
+    private float x;
+
+    public task1(float x){
+        this.x = x;
+    }
+    public double calculate(){
+        float y = 2 * this.x - 1;
+        return y;
+    }
+}
