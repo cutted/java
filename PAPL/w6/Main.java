351b8032652a00e7d4ccf83e427f128b46b35c53
Jono <30672425+cutted@users.noreply.github.com>
2019-11-08 16:07:24 +0000

    Create Main.java

---
+++ PAPL/w6/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        task1 t1 = new task1();
+        System.out.println(t1.task1());
+        task2 t2 = new task2();
+        System.out.println(t2.task2());
+        task3 t3 = new task3();
+        System.out.println(t3.task3());
+
+        task4 t4 = new task4();
+        System.out.println(t4.task4());
+        task5 t5 = new task5();
+        System.out.println(t5.task5());
+
+        task6 t6 = new task6();
+        System.out.println(t6.task6());
+        task7 t7 = new task7();
+        System.out.println(t7.task7());
+    }
+
+}
