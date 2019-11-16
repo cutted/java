679b914b3de803af2b537d3debcb9870773c59a5
Jono <30672425+jonathoncarter@users.noreply.github.com>
2019-11-16 23:40:43 +0000

    Create task3.java

---
+++ PAPL/02 Objects & Classes/task3.java
@@ -0,0 +1,17 @@
+public class task3 {
+
+    private int iVal;
+    private String sVal;
+    private Boolean bVal;
+
+    // (1, =, true)
+    public task3(int inInt, String inStr, Boolean inBool){
+        iVal = inInt;
+        sVal = inStr;
+        bVal = inBool;
+    }
+
+    public int getIVal(){ return iVal; }
+    public String getSVal(){ return sVal; }
+    public Boolean getBVal(){ return bVal;}
+}
